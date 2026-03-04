37447fbfdb469e1a63e8c4f62923c5459de41773
floyare <neptuneeex@gmail.com>
2026-03-04 14:02:26 +0100

    feat: add racing-input prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import RacingInput from "@/components/racing-input";
+
 export default function HomePage() {
     return (
         <main className="">
-
+            <RacingInput />
         </main>
     );
 }

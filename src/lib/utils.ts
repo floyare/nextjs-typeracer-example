117f4362cbf167613dca23bc6dfe3930c693ff2a
floyare <neptuneeex@gmail.com>
2026-03-05 19:01:02 +0100

    chore: minor changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,13 +2,14 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
 
 // AI generated, probably better to just use uuidv4 package but im leaving this
 export function generateId() {
-  if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
-    return crypto.randomUUID();
-  }
-  return Math.random().toString(36).substring(2) + Date.now().toString(36);
+    if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
+        return crypto.randomUUID();
+    }
+
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
 }
